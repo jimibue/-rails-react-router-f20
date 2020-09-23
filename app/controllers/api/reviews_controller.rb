8f7dae66cc56f1c7a9e3c2157996f30fbf26d9b9
James Yeates <jimibue@gmail.com>
2020-09-22 18:12:33 -0600

    delete for reviews

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -1,4 +1,7 @@
 class Api::ReviewsController < ApplicationController
+  before_action :set_product
+  before_action :set_review, only: [:destroy]
+
   def index
   end
 
@@ -6,11 +9,33 @@ class Api::ReviewsController < ApplicationController
   end
 
   def create
+    review = @product.reviews.new(review_params)
+    if (review.save)
+      render json: review
+    else
+      render json: review.errors, status: 422
+    end
   end
 
   def update
   end
 
   def destroy
+    review = @review.destroy()
+    render json: review
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:text, :product_id)
+  end
+
+  def set_product
+    @product = Product.find(params[:product_id])
+  end
+
+  def set_review
+    @review = @product.reviews.find(params[:id])
   end
 end
--- client/src/products/ProductView.js
+++ client/src/products/ProductView.js
@@ -1,17 +1,44 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Button, Header, Segment } from "semantic-ui-react";
+import { Button, Header, Segment, Form } from "semantic-ui-react";
 
 const ProductView = ({ match, history }) => {
   const [product, setProduct] = useState({});
+  const [text, setText] = useState("");
   useEffect(() => {
     axios.get(`/api/products/${match.params.id}`).then((res) => {
-      debugger;
       setProduct(res.data);
     });
   }, []);
+  function createReview() {
+    axios
+      .post(`/api/products/${product.id}/reviews`, { text })
+      .then((res) => {
+        setText("");
+        setProduct({ ...product, reviews: [...product.reviews, res.data] });
+      })
+      .catch((e) => {
+        alert("error in create");
+      });
+  }
 
-  const { name, description, price, department, reviews = [] } = product;
+  function handleSubmit() {
+    createReview();
+  }
+  async function deleteReview(id) {
+    console.log("yo");
+    try {
+      let res = await axios.delete(`/api/products/${product.id}/reviews/${id}`);
+      setProduct({
+        ...product,
+        reviews: product.reviews.filter((p) => p.id !== res.data.id),
+      });
+    } catch (err) {
+      alert("delete failed");
+    }
+  }
+
+  const { name, description, price, department, id, reviews = [] } = product;
 
   return (
     <div>
@@ -24,8 +51,21 @@ const ProductView = ({ match, history }) => {
         <p>{description}</p>
         <Header as="h3">Reviews</Header>
         {reviews.map((review) => (
-          <p key={review.id}>{review.text}</p>
+          <p key={review.id}>
+            {review.text}{" "}
+            <span onClick={() => deleteReview(review.id)}> &#128465;</span>
+            <span onClick={() => deleteReview(review.id)}>&#9998;</span>
+          </p>
         ))}
+        <h3>Add Review</h3>
+        <Form onSubmit={handleSubmit}>
+          <Form.Input
+            value={text}
+            label="Review"
+            required
+            onChange={(e) => setText(e.target.value)}
+          />
+        </Form>
       </Segment>
       <br />
       <br />
